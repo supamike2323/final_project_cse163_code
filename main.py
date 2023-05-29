26a2cbb06525436bb43d0fea52f415e0cf5334b4
supamike2323 <68355268+supamike2323@users.noreply.github.com>
2023-05-29 15:27:13 -0700

    ss

---
+++ main.py
@@ -0,0 +1,12 @@
+import pandas
+
+def main():
+    df = pandas.read_csv('country_vaccinations.csv')
+    print("hello world")
+    print("hello 1world")
+
+
+
+if __name__ == '__main__':
+    main()
+
